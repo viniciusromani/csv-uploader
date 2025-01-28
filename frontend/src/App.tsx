13c7c70b8cbcd965d0aa480c212a31ab09ff53c2
Vinicius Romani <vn.romani@gmail.com>
2025-01-28 16:59:49 -0300

    creating initial UI and implementing basic structure to send files to backend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,73 @@
-import { Button } from './components/ui/button'
+import { useState } from 'react'
+import { z } from 'zod'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { 
+  Button,
+  Input,
+  Progress,
+  Form, 
+  FormControl, 
+  FormField, 
+  FormItem, 
+  FormLabel, 
+  FormMessage
+} from '@/components/shadcn'
+
+const schema = z.object({
+  file: z
+    .instanceof(FileList)
+    .refine((file) => file?.length == 1, 'File is required.')
+})
+type FormSchema = z.infer<typeof schema>
 
 function App() {
+  const [progress, setProgress] = useState(0)
+
+  const form = useForm<FormSchema>({
+    resolver: zodResolver(schema)
+  })
+  const onSubmit = async (data: FormSchema) => {
+    const formData = new FormData()
+    formData.append('file', data.file[0])
+
+    try {
+      console.log(process.env.API_URL)
+      console.log('upload file', formData)
+    } catch (error) {
+      console.error('Error uploading file:', error)
+    }
+  }
+
   return (
     <>
-      <h1>Vite + React</h1>
-      <div>Hello World</div>
-      <div>{ process.env.API_URL }</div>
-      <Button>Click me!</Button>
+      <h1>Upload CSV</h1>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 p-10">
+          <FormField
+            control={form.control}
+            name="file"
+            render={({ field }) => {
+              return (
+                <FormItem>
+                  <FormLabel>Choose CSV</FormLabel>
+                  <FormControl>
+                    <Input 
+                      type="file" 
+                      accept=".csv"
+                      onChange={(e) => field.onChange(e.target.files)}
+                      ref={field.ref}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              );
+            }}
+          />
+          <Progress value={progress} />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
     </>
   )
 }
---
+++ frontend/src/components/shadcn/index.ts
@@ -0,0 +1,12 @@
+export { Input } from './input'
+export { Button } from './button'
+export { Progress } from './progress'
+export { 
+  Form,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormDescription,
+  FormMessage,
+  FormField
+} from './form'
